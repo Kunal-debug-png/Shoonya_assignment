0af3c5d55e7936ce1143540d98a516a49cb6096a
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-24 16:46:55 +0530

    Initial commit

---
+++ shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/ShoonyaApplication.java
@@ -0,0 +1,13 @@
+package com.kunal_debug_png.shoonya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoonyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoonyaApplication.class, args);
+	}
+
+}
---
+++ shoonya/shoonya/src/test/java/com/kunal_debug_png/shoonya/ShoonyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal_debug_png.shoonya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoonyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
