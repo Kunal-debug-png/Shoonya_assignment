ad032a32c282286cc7481f7515cdb83569da15d7
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-26 02:48:27 +0530

    Bookings handler

--- shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/controller/BookingController.java
+++ shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/controller/BookingController.java
@@ -1,2 +1,25 @@
-package com.kunal_debug_png.shoonya.controller;public class BookingController {
+package com.kunal_debug_png.shoonya.controller;
+
+import com.kunal_debug_png.shoonya.entity.Booking;
+import com.kunal_debug_png.shoonya.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookings")
+public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping("/book")
+    public Booking createBooking(@RequestBody Booking booking) {
+        return bookingService.createBooking(booking);
+    }
+
+    @GetMapping
+    public List<Booking> getAllBookings(){
+        return bookingService.getAll();
+    }
 }
