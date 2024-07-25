fc625d0d4bea5f73f993d3b432b4b543f6428a62
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-26 02:55:28 +0530

    Final commit

--- shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/service/BookingService.java
+++ shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/service/BookingService.java
@@ -1,2 +1,32 @@
-package com.kunal_debug_png.shoonya.service;public class BookingService {
+package com.kunal_debug_png.shoonya.service;
+
+
+
+import com.kunal_debug_png.shoonya.entity.Booking;
+import com.kunal_debug_png.shoonya.repository.BookingRepository;
+import com.kunal_debug_png.shoonya.repository.RetreatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+
+
+@Service
+public class BookingService {
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public Booking createBooking(Booking booking) {
+        if (bookingRepository.existsByUserIdAndRetreatId(booking.getUserId(), booking.getRetreatId())) {
+            throw new RuntimeException("Retreat already booked by the user");
+        }
+        return bookingRepository.save(booking);
+    }
+
+    public List<Booking> getAll() {
+        return bookingRepository.findAll();
+    }
 }
