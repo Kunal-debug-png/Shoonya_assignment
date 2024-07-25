f84f33b93c07d95004d0cd1036d4783e7a39ca95
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-26 02:52:06 +0530

    Booking logic

--- shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/repository/BookingRepository.java
+++ shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/repository/BookingRepository.java
@@ -1,2 +1,14 @@
-package com.kunal_debug_png.shoonya.repository;public class BookingRepository {
+package com.kunal_debug_png.shoonya.repository;
+
+
+import com.kunal_debug_png.shoonya.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    boolean existsByUserIdAndRetreatId(Long userId, Long retreatId);
 }
