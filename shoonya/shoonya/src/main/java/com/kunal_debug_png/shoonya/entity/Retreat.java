e14f8f3bf561a08c369dc832b86553e8c74fa7ed
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-25 22:38:08 +0530

    Retreat Model

--- shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/entity/Retreat.java
+++ shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/entity/Retreat.java
@@ -1,2 +1,37 @@
-package com.kunal_debug_png.shoonya.entity;public class Retreat {
+package com.kunal_debug_png.shoonya.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Retreat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Long date;
+    private String location;
+    private int price;
+    private String type;
+    private String condition;
+    private String image;
+    private int duration;
+    @ElementCollection
+    private List<String> tag;
+
 }
