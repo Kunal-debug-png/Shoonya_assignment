d2de212fa3df46448f48e25437ce35a308327f30
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-26 02:50:10 +0530

    Booking Entity

--- shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/entity/Booking.java
+++ shoonya/shoonya/src/main/java/com/kunal_debug_png/shoonya/entity/Booking.java
@@ -15,16 +15,43 @@ import lombok.Data;
 @AllArgsConstructor
 @Entity
 public class Booking {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_id")
     private Long userId;
+
+    @Column(name = "user_name")
     private String userName;
+
+    @Column(name = "user_email")
     private String userEmail;
+
+    @Column(name = "user_phone")
     private String userPhone;
-    @ManyToOne
-    @JoinColumn(name = "retreat_id")
-    private Retreat retreat;
+
+    @Column(name = "retreat_id") // Use a consistent name here
+    private Long retreatId;
+
+    @Column(name = "retreat_title")
+    private String retreatTitle;
+
+    @Column(name = "retreat_location")
+    private String retreatLocation;
+
+    @Column(name = "retreat_price")
+    private Integer retreatPrice;
+
+    @Column(name = "retreat_duration")
+    private Integer retreatDuration;
+
+    @Column(name = "payment_details")
     private String paymentDetails;
+
+    @Column(name = "booking_date")
     private Long bookingDate;
+
+
 }
